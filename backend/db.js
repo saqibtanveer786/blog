bbbd479d17d7d8009c384a0ccbcd6581f410e4ed
Saqib Tanveer <adward797@gmail.com>
2023-04-04 20:06:00 -0700

    com

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 module.exports = async function connectToMongo() {
   await mongoose.connect(
-    process.env.REACT_APP_API_URL || `mongodb://127.0.0.1:27017`
+    process.env.REACT_APP_API_URL ||
+      `mongodb+srv://saqib:saqib123@blog.oalntnk.mongodb.net/?retryWrites=true&w=majority`
   );
   console.log('connected to mongo');
 };
